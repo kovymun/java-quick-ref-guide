3c07d947dd4ec9265288ff8ee761d575df8a3eda
Kovy <codingstudent2021@gmail.com>
2024-10-07 22:27:30 +0200

    Initial Commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,27 @@
+/*
+ * JAVA PROGRAM ENTRY POINT:
+ * In this section, we will explore the entry point of a Java program and guide you through writing your first Java program.
+ * Our first program will be the classic "Hello World!" which will demonstrate the basic structure and functionality of a Java application.
+ * We will break down concepts such as the class declaration, the main method, and how to print output to the console.
+ */
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+ ---------
+ * NOTES *
+ ---------
+
+ * JAVA STATEMENT: In Java, a "statement" is a line of code terminated with a semicolon ";".
+ * CLASS DECLARATION: Every Java program begins with a class declaration. The class name must match the filename, and should always start with an "Uppercase first letter".
+ * MAIN() METHOD: Every standalone Java application must have a "main()" method, which serves as the entry point for the program.
+ * ACCESS MODIFIERS: The "public" keyword makes the class and method accessible from anywhere. Other access modifiers include: private | protected.
+ * STATIC METHOD: The "static" keyword allows the main method to be called without creating an instance of the class.
+ * VOID: The "void" keyword specifies that the "main()" method does not return any value.
+ * STRING[] ARGS: This parameter stores any command-line arguments passed to the program.
+ * PRINTING TO CONSOLE: The `System.out.println()` method is used to print text to the console followed by a new line.
+ */
