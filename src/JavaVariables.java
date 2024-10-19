d7939541671c3fb29ff54110651543f7f0619f50
Kovy <codingstudent2021@gmail.com>
2024-10-19 20:53:16 +0200

    Adding Java Variables Comments

--- src/JavaVariables.java
+++ src/JavaVariables.java
@@ -1,7 +1,19 @@
+/*
+---------------
+JAVA VARIABLES:
+---------------
+
+* A variable (in any programming language) can be described as a named container that stores a piece of data in memory.
+* SYNTAX: data-type *identifier-name* = initialized value;
+* EXAMPLE: String fName = "James";
+* Naming convention standard used for Java: CAMEL CASE
+*/
+
 public class JavaVariables {
     public static void main(String[] args) {
-        String name = "Kyle"; //variable name assigned value "kyle" of data Type String
-        int age = 25; //variable name assigned value 25 of data Type integer (int)
-        System.out.println(name + ", Age:" + age); //string concatenation
+        String fName = "Kyle"; // variable 'fName' assigned the value "Kyle", of data type String
+        int age = 25; // variable 'age' assigned the value 25, of data type int (integer)
+        System.out.println("Hi I'm " + fName + " and I'm " + age + " years old!"); // string concatenation to display the output
     }
 }
+
