0a850ddf1045ad288ffa7b256bb615c7df437141
Kovy <codingstudent2021@gmail.com>
2024-10-19 18:31:15 +0200

    Adding Java Variables Concepts

---
+++ src/JavaVariables.java
@@ -0,0 +1,7 @@
+public class JavaVariables {
+    public static void main(String[] args) {
+        String name = "Kyle"; //variable name assigned value "kyle" of data Type String
+        int age = 25; //variable name assigned value 25 of data Type integer (int)
+        System.out.println(name + ", Age:" + age); //string concatenation
+    }
+}
